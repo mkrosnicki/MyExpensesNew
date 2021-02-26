513f053b5bc77ebe61ed462df05bf87aaa9e51fa
mkrosnicki <maciejkrosnicki@gmail.com>
2021-02-26 11:08:14 +0100

    landscape mode support - media query wrapped into a var

--- lib/main.dart
+++ lib/main.dart
@@ -113,7 +113,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscapeMode = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscapeMode = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text(
         'My Expenses',
@@ -129,9 +130,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     final txListWidget = Container(
-      height: (MediaQuery.of(context).size.height -
+      height: (mediaQuery.size.height -
           appBar.preferredSize.height -
-          MediaQuery.of(context).padding.top) *
+          mediaQuery.padding.top) *
           0.7,
       child:
       TransactionList(_userTransactions, _deleteTransaction),
@@ -158,18 +159,18 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
             if (!isLandscapeMode) Container(
-              height: (MediaQuery.of(context).size.height -
+              height: (mediaQuery.size.height -
                   appBar.preferredSize.height -
-                  MediaQuery.of(context).padding.top) *
+                  mediaQuery.padding.top) *
                   0.3,
               child: Chart(_recentTransactions),
             ),
             if (!isLandscapeMode) txListWidget,
             if (isLandscapeMode) _showChart
                 ? Container(
-                    height: (MediaQuery.of(context).size.height -
+                    height: (mediaQuery.size.height -
                             appBar.preferredSize.height -
-                            MediaQuery.of(context).padding.top) *
+                            mediaQuery.padding.top) *
                         0.7,
                     child: Chart(_recentTransactions),
                   )
